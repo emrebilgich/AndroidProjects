95fa98814915f7cc2daf48134360089890afd9c3
Yunus Emre Bilgiç <yunusbilgic09@gmail.com>
2025-03-01 15:17:18 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,36 +1,36 @@
-package com.bilgic.alertdialog;
+package com.bilgic.countdowntimer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Toast Message", Toast.LENGTH_LONG).show();
+        textView = findViewById(R.id.textView);
+        new CountDownTimer(10000, 1000){
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onTick(long millisUntilFinished) {
+            textView.setText("Left" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
+            }
+        }.start();
+
+        }
     }
-
-    public void save(View view) {
-        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-
-        alert.setTitle("Save");
-        alert.setMessage("Are you sure?");
-        alert.setPositiveButton("Yes", (dialog, which) -> {
-            Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
-            dialog.dismiss();
-        });
-
-        alert.setNegativeButton("No", (dialog, which) -> {
-            Toast.makeText(MainActivity.this, "Not Saved", Toast.LENGTH_LONG).show();
-            dialog.dismiss();
-        });
-
-        alert.show();
-    }
-}
